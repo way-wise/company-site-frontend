e3c2e73e2d17274d5a9a543f8465ea6de891376e
sakil-sagor <sakilsagor76543@gmail.com>
2025-09-24 18:39:14 +0600

    make folder structer

---
+++ src/middleware.ts
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getCurrentUser } from "./services/AuthService";
+
+type Role = keyof typeof roleBasedPrivateRoutes;
+
+const authRoutes = ["/login", "/register"];
+
+const roleBasedPrivateRoutes = {
+  user: [/^\/user/, /^\/create-shop/],
+  admin: [/^\/admin/],
+};
+
+export const middleware = async (request: NextRequest) => {
+  const { pathname } = request.nextUrl;
+
+  const userInfo = await getCurrentUser();
+
+  if (!userInfo) {
+    if (authRoutes.includes(pathname)) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(
+        new URL(
+          `http://localhost:3000/login?redirectPath=${pathname}`,
+          request.url
+        )
+      );
+    }
+  }
+
+  if (userInfo?.role && roleBasedPrivateRoutes[userInfo?.role as Role]) {
+    const routes = roleBasedPrivateRoutes[userInfo?.role as Role];
+    if (routes.some((route) => pathname.match(route))) {
+      return NextResponse.next();
+    }
+  }
+
+  return NextResponse.redirect(new URL("/", request.url));
+};
+
+export const config = {
+  matcher: [
+    "/login",
+    "/create-shop",
+    "/admin",
+    "/admin/:page",
+    "/user",
+    "/user/:page",
+  ],
+};
