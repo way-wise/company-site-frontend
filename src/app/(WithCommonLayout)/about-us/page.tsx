f2369a2ae6cad2b3b6e5393c9a93535d38a75f50
sakil-sagor <sakilsagor76543@gmail.com>
2025-09-26 17:42:00 +0600

    fix about us

--- src/app/(WithCommonLayout)/about-us/page.tsx
+++ src/app/(WithCommonLayout)/about-us/page.tsx
@@ -1,7 +1,24 @@
 import ChooseUs from "@/components/modules/aboutUs/whychooseUs";
+import CategorySection from "@/components/modules/home/CategorySection";
+import ContactUs from "@/components/modules/home/ContactUs";
+import SuccessProjects from "@/components/modules/services/SuccessProjects";
+import PageHeader from "@/components/shared/PageHeader";
 
 const AboutUs = () => {
-  return <ChooseUs />;
+  return (
+    <>
+      <PageHeader
+        title="ABOUT"
+        description="WAY-WISE"
+        titleClass="text-white text-5xl lg:text-[85px] font-bold font-['Akira Expanded]"
+        descriptionClass="text-brand text-5xl lg:text-[85px]"
+      />
+      <ChooseUs />
+      <SuccessProjects />
+      <CategorySection />
+      <ContactUs />
+    </>
+  );
 };
 
 export default AboutUs;
