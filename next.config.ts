417ead718f0e66db3a48e036efb4107dc92ce488
sakil-sagor <sakilsagor76543@gmail.com>
2025-09-29 14:05:06 +0600

    fix heor section

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    optimizePackageImports: ["framer-motion"],
+  },
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
--- src/components/modules/home/HeroSection.tsx
+++ src/components/modules/home/HeroSection.tsx
@@ -1,59 +1,72 @@
-import mainBackground from "@/assets/images/home/hero-bg.png";
+import mainBackground from "@/assets/images/home/full-bg.png";
+import heroLeftBg from "@/assets/images/home/hero-ai.png";
+import Image from "next/image";
 
 const HeroSection = () => {
+  console.log(heroLeftBg.src);
   return (
     <section
-      className="w-full h-full py-10 lg:h-[80vh]"
+      className="w-full h-full py-10 lg:h-[80vh] bg-left  md:bg-right lg:bg-center"
       style={{
         backgroundImage: `url(${mainBackground.src})`,
         backgroundSize: "cover",
-        backgroundPosition: "center",
+        // backgroundPosition: "right",
         backgroundRepeat: "no-repeat",
       }}
     >
       {/* Main content */}
-      <div className="  flex flex-col items- justify-center h-full   px-4 text-center">
-        <div className="max-w-5xl mx-auto">
+
+      <div className="container mx-auto px-2 grid grid-cols-1 md:grid-cols-2 h-full">
+        <div className="flex flex-col justify-center items-center md:items-start">
           {/* Main heading */}
           <h1 className="mb-8">
-            <div className="text-4xl md:text-6xl lg:text-8xl font-bold tracking-wide">
-              <p className="text-white ">INSPIRING</p>
-              <p className="text-transparent bg-gradient-to-r from-[#00D4FF] to-[#0099CC] bg-clip-text ">
-                INNOVATION
-              </p>
-              <p className="text-white">WITH AI</p>
+            <div className="text-4xl lg:text-6xl  font-bold tracking-wide text-center md:text-left">
+              <h1 className="text-white ">WAY-WISE</h1>
+              <h2 className="text-transparent bg-gradient-to-r from-[#00D4FF] to-[#0099CC] bg-clip-text ">
+                TECHNOLOGY
+              </h2>
             </div>
           </h1>
 
           {/* Description */}
-          <p className="text-white/80 text-md md:text-xl lg:text-2xl max-w-3xl mx-auto mb-12 leading-relaxed">
-            We are a global software development firm delivering
-            <br />
-            innovative, customized solutions that empower businesses
-            <br />
-            and set new standards in quality and technology.
+          <p className="text-white/80 text-md  lg:text-2xl max-w-3xl mb-12 leading-relaxed text-center  md:text-left ">
+            We are a global software development firm delivering innovative,
+            customized solutions that empower businesses and set new standards
+            in quality and technology.
           </p>
 
           {/* CTA Button */}
-          <button className="bg-gradient-to-r from-[#00D4FF] to-[#0099CC] text-white px-8 py-4 rounded-lg text-lg font-semibold hover:shadow-2xl hover:shadow-cyan-500/25 transition-all duration-300 transform hover:scale-105 flex items-center gap-2 mx-auto">
-            Let&apos;s Get Started
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="ml-2"
-            >
-              <path
-                d="M5 12H19M19 12L12 5M19 12L12 19"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </button>
+          <div>
+            <button className="bg-gradient-to-r from-[#00D4FF] to-[#0099CC] text-white lg:px-8 lg:py-4 px-4 py-3 rounded-lg text-lg font-semibold hover:shadow-2xl hover:shadow-cyan-500/25 transition-all duration-300 transform hover:scale-105 flex items-center gap-2 text-left">
+              Let&apos;s Get Started
+              <svg
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="ml-2"
+              >
+                <path
+                  d="M5 12H19M19 12L12 5M19 12L12 19"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div className="flex justify-end items-center">
+          <div className="animate-bounce-slow">
+            <Image
+              src={heroLeftBg.src}
+              alt="hero left bg"
+              width={400}
+              height={400}
+            />
+          </div>
         </div>
       </div>
     </section>
