952d5bfba479ea4919aeb216fe4bc240d29371e2
sakil-sagor <sakilsagor76543@gmail.com>
2025-10-09 19:51:28 +0600

    protect user table

--- src/app/(WithDashboardLayout)/admin/users/page.tsx
+++ src/app/(WithDashboardLayout)/admin/users/page.tsx
@@ -1,7 +1,15 @@
+import { PermissionGuard } from "@/components/auth/PermissionGuard";
 import { UsersTable } from "../_components/user-components/users-table";
 
 const UsersPage = async () => {
-  return <UsersTable />;
+  return (
+    <PermissionGuard
+      permissions={["read_role", "create_role"]}
+      requireAll={false}
+    >
+      <UsersTable />
+    </PermissionGuard>
+  );
 };
 
 export default UsersPage;
