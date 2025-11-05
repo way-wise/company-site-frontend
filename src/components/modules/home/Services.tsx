4cc31774d78301f4deff4fb5c7578c8617f2b8b8
rakib0101 <rakib.01jan@gmail.com>
2025-11-05 15:36:52 +0600

    add: microsoft customer support part

--- src/components/modules/home/Services.tsx
+++ src/components/modules/home/Services.tsx
@@ -1,8 +1,11 @@
 import SectionTitle from "@/components/modules/home/SectionTitle";
 import { ServiceDisplay } from "@/types";
 import ServiceCard from "./ServiceCard";
+import Image from "next/image";
+import microsoftSupport from "@/assets/images/services/microsoft-support.png";
 
 const Services = ({ services }: { services: ServiceDisplay[] }) => {
+
   return (
     <section className="  bg-[url('@/assets/images/services/service-bg.png')] bg-cover bg-center bg-no-repeat">
       <div className="container mx-auto py-12 lg:py-20 ">
@@ -15,6 +18,25 @@ growth, enhance your brand, and streamline your operations."
             titleClass="text-white text-4xl pb-4  xl:text-[55px] font-bold"
             descriptionClass="text-white text-base xl:text-[20px]"
           />
+        </div>
+        <div className="flex justify-center items-center">
+          <div className="max-w-4xl p-8 flex gap-8 justify-between items-center bg-linear-to-tr from-[#638f06] to-[#e23808] rounded-xl mb-8">
+            <div>
+              <h2 className="text-white text-2xl font-bold mb-4">Microsoft M365 Customer Support Services</h2>
+                <p className="text-white text-base mb-1">Way-Wise Tech provides end-to-end Microsoft 365 support covering:
+                  </p>
+                <ul className="list-disc text-sm list-inside text-white">
+                  <li>SCIM / Identity & Access Management</li>
+                  <li>SharePoint Online (SPO)</li>
+                  <li>Exchange (Online and On-Prem migrations)</li>
+                  <li>Microsoft Teams</li>
+                  <li>Azure Services</li>
+                  <li>Billing & Subscription Management</li>
+                  <li>Forum / Community / Support Desk Operations</li>
+                </ul>
+            </div>
+            <Image src={microsoftSupport} alt="Microsoft Support" className="w-1/2 rounded-md" />
+          </div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 xl:gap-20  ">
           {services?.map((service) => (
