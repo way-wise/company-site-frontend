2d9b1a159ca4d825b45bb140d3fac267efbb7ced
rakib0101 <rakib.01jan@gmail.com>
2025-11-05 20:55:51 +0600

    wip

--- src/components/modules/services/MicrosoftSupportDetails.tsx
+++ src/components/modules/services/MicrosoftSupportDetails.tsx
@@ -362,6 +362,11 @@ const MicrosoftSupportDetails = () => {
             </div>
           </div>
 
+
+        </div>
+      </section>
+      <section className="py-8 md:py-10 bg-white">
+        <div className="container">
           <div className="bg-[#f2f6ff] rounded-lg p-6 md:p-8 shadow-sm max-w-5xl mx-auto mt-12 flex flex-col items-center justify-center">
             <h2 className="text-lg lg:text-2xl text-center font-semibold text-[#1b3447] mb-4 md:mb-8">SUPPORT DELIVERY FRAMEWORK</h2>
             <Image src={supportDeliveryFramework} alt="Support Delivery Framework" />
