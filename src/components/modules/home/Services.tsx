7daf2b69db82dde062c887883a8f2cd1363a2f41
A K M Sakil Sagor <sakilsagor76543@gmail.com>
2025-11-19 10:48:25 +0600

    done seo

--- src/components/modules/home/Services.tsx
+++ src/components/modules/home/Services.tsx
@@ -1,3 +1,4 @@
+import medicalBilling from "@/assets/images/services/billing-shape.png";
 import microsoftSupport from "@/assets/images/services/ms-support.png";
 import SectionTitle from "@/components/modules/home/SectionTitle";
 import { ServiceDisplay } from "@/types";
@@ -17,9 +18,10 @@ const Services = ({
       <div className="container mx-auto py-12 lg:py-20 ">
         <div className="mb-16">
           <SectionTitle
-            title="Web Development, Software Development & Digital Solution Services"
-            description="We are a reputable web development firm in the USA that offers a full range of digital and software development services that are customized to match the needs of your business. Our team of experts creates custom websites, apps, and business solutions that help your business expand, improve your brand identity, and make your operations run more smoothly. Way Wise Tech helps you transform your ideas into digital success that can grow, whether you're a new business or an established one.
-"
+            title="Our Services"
+            description="We offer a comprehensive suite of services designed to meet all your digital needs.
+Our team of experts is committed to delivering top-notch solutions that drive
+growth, enhance your brand, and streamline your operations."
             titleClass="text-white text-4xl pb-4  xl:text-[55px] font-bold"
             descriptionClass="text-white text-base xl:text-[20px]"
           />
@@ -30,27 +32,18 @@ const Services = ({
             <ServiceCard key={service.id} service={service} />
           ))}
         </div>
-        <div className="flex justify-center items-center my-12">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 my-12">
           <div className="w-full max-w-5xl flex flex-col lg:flex-row gap-6 lg:gap-8 justify-between bg-white rounded-xl relative overflow-hidden">
-            <div className="lg:w-1/2 p-5 lg:p-12">
+            <div className="lg:w-1/2 p-5 lg:p-6">
               <h2 className="text-black text-2xl font-bold mb-4">
                 Microsoft M365 Customer Support <br />
                 <span className="text-lg tetx-brand">
                   (Services Previously Provided by our tech)
                 </span>
               </h2>
-              <p className="text-gray-900 font-semibold text-sm mb-1">
+              <p className="text-gray-900 text-sm mb-1">
                 Way-Wise Tech covered end-to-end Microsoft 365 support:
               </p>
-              <ul className="list-disc text-sm list-inside text-gray-800">
-                <li>SCIM / Identity & Access Management</li>
-                <li>SharePoint Online (SPO)</li>
-                <li>Exchange (Online and On-Prem migrations)</li>
-                <li>Microsoft Teams</li>
-                <li>Azure Services</li>
-                <li>Billing & Subscription Management</li>
-                <li>Forum / Community / Support Desk Operations</li>
-              </ul>
               <Link
                 href="/microsoft-support"
                 className="inline-block text-white bg-brand px-4 py-2 rounded-md text-base mt-3"
@@ -61,7 +54,33 @@ const Services = ({
             <Image
               src={microsoftSupport}
               alt="Microsoft Support"
-              className="h-full w-auto lg:absolute right-0 bottom-0"
+              className="lg:max-w-1/2 h-full w-full lg:w-auto lg:absolute right-0 bottom-0"
+            />
+          </div>
+          <div className="w-full max-w-5xl flex flex-col lg:flex-row gap-6 lg:gap-8 justify-between bg-white rounded-xl relative overflow-hidden">
+            <div className="lg:w-1/2 p-5 lg:p-6">
+              <h2 className="text-black text-2xl font-bold mb-4">
+                Medical Billing & Coding Services <br />
+                {/* <span className="text-lg tetx-brand">
+                  (Services Previously Provided by our tech)
+                </span> */}
+              </h2>
+              <p className="text-gray-900 text-sm mb-1">
+                Let Us Take Care of Your Revenue While You Take Care of Your
+                Patients. Expert medical billing, medical coding, revenue cycle
+                management, credentialing, and compliance services.
+              </p>
+              <Link
+                href="/medical-it-support"
+                className="inline-block text-white bg-brand px-4 py-2 rounded-md text-base mt-3"
+              >
+                Learn More
+              </Link>
+            </div>
+            <Image
+              src={medicalBilling}
+              alt="Microsoft Support"
+              className="lg:max-w-1/2 h-full w-full lg:w-auto lg:absolute right-0 bottom-0"
             />
           </div>
         </div>
